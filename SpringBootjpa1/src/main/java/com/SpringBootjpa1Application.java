410ba91d38ec4970f84abd01b9d6271c5ec8cca3
RISHI <RISHI@DESKTOP-0OOCA2N>
2021-08-04 15:32:42 +0530

    MY FIRST COMMIT

---
+++ SpringBootjpa1/src/main/java/com/SpringBootjpa1Application.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringBootjpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootjpa1Application.class, args);
+		System.out.println("Cnnected to Database ");
+	}
+
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com")).build();
+	   }
+	
+	//http://localhost:9090/swagger-ui/index.html
+}
